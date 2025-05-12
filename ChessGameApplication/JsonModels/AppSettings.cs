d4a0e597ca798ca5238141ca3cd11ff9776235d7
Shadocl-low <vt231_poi@student.ztu.edu.ua>
2025-05-12 21:37:56 +0300

    Created AppSettings model for json file

---
+++ ChessGameApplication/JsonModels/AppSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessGameApplication.JsonModels
+{
+    public class AppSettings
+    {
+        public string Theme { get; set; } = "Light";
+        public string WindowMode { get; set; } = "Windowed";
+    }
+}
