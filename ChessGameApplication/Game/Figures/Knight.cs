7cea82125e21240405cf4f149a24b146e89fc0e6
Shadocl-low <vt231_poi@student.ztu.edu.ua>
2025-05-13 20:03:49 +0300

    Created Knight class

---
+++ ChessGameApplication/Game/Figures/Knight.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessGameApplication.Game.Figures
+{
+    public class Knight : Piece
+    {
+        public Knight(PieceColor color, Position position) : base(color, position) { }
+
+        public override List<Position> GetAvailableMoves(Board board)
+        {
+            var moves = new List<Position>();
+
+            int[][] offsets = 
+            {
+                [2, 1], [2, -1], [-2, 1], [-2, -1],
+                [1, 2], [1, -2], [-1, 2], [-1, -2]
+            };
+
+            foreach (var offset in offsets)
+            {
+                var pos = Position.Add(offset[0], offset[1]);
+                if (board.IsInsideBoard(pos) && (board.IsEnemyPiece(pos, Color) || board.IsEmpty(pos)))
+                    moves.Add(pos);
+            }
+
+            return moves;
+        }
+    }
+}
