46f93132d5664629a5c95900b5cf048a8c2975d0
Shadocl-low <vt231_poi@student.ztu.edu.ua>
2025-05-13 16:46:40 +0300

    Created interface for WindowManager for implementing Mediator pattern. Created WindowManager class, that contains current window and objects of all others windows. Manager have Notify method for switching windows

---
+++ ChessGameApplication/Windows/Manager/IWindowManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ChessGameApplication.Windows.Manager
+{
+    public interface IWindowManager
+    {
+        void Notify(Window sender, string action);
+    }
+}
---
+++ ChessGameApplication/Windows/Manager/WindowManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ChessGameApplication.Windows.Manager
+{
+    public class WindowManager : IWindowManager
+    {
+        private Window? CurrentWindow;
+
+        private readonly GameWindow gameWindow;
+        private readonly MainMenuWindow mainMenuWindow;
+        private readonly SettingsWindow settingsWindow;
+
+        public WindowManager()
+        {
+            gameWindow = new GameWindow(this);
+            mainMenuWindow = new MainMenuWindow(this);
+            settingsWindow = new SettingsWindow(this);
+        }
+        public void Notify(Window sender, string action)
+        {
+            switch (action)
+            {
+                case "OpenMainMenu":
+                    SwitchWindow(mainMenuWindow);
+                    break;
+                case "OpenGame":
+                    SwitchWindow(gameWindow);
+                    break;
+                case "OpenSettings":
+                    SwitchWindow(settingsWindow);
+                    break;
+                case "Exit":
+                    Application.Current.Shutdown();
+                    break;
+                default:
+                    throw new ArgumentException($"Невідома дія: {action}");
+            }
+        }
+
+        private void SwitchWindow(Window newWindow)
+        {
+            if (CurrentWindow == newWindow)
+                return;
+
+            CurrentWindow?.Hide();
+            CurrentWindow = newWindow;
+            CurrentWindow.Show();
+        }
+    }
+}
