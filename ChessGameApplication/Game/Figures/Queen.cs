099619e5776e232390708f92467a60fab8354df6
Shadocl-low <vt231_poi@student.ztu.edu.ua>
2025-05-13 20:14:16 +0300

    Created Queen piece

---
+++ ChessGameApplication/Game/Figures/Queen.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessGameApplication.Game.Figures
+{
+    public class Queen : Piece
+    {
+        public Queen(PieceColor color, Position position) : base(color, position) { }
+
+        public override List<Position> GetAvailableMoves(Board board)
+        {
+            return board.GetMovesInDirections(Position, Color, [
+                (1, 0), (-1, 0), (0, 1), (0, -1),
+                (1, 1), (1, -1), (-1, 1), (-1, -1)
+            ]);
+        }
+    }
+}
