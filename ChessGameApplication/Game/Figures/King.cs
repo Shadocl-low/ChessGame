cf3fa96aca69d750ddd97536a9a0bf5b2ee6664e
Shadocl-low <vt231_poi@student.ztu.edu.ua>
2025-05-13 20:20:10 +0300

    Created King Piece

---
+++ ChessGameApplication/Game/Figures/King.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessGameApplication.Game.Figures
+{
+    public class King : Piece
+    {
+        public King(PieceColor color, Position position) : base(color, position) { }
+
+        public override List<Position> GetAvailableMoves(Board board)
+        {
+            var moves = new List<Position>();
+
+            var directions = new[] {
+                (1, 0), (-1, 0), (0, 1), (0, -1),
+                (1, 1), (1, -1), (-1, 1), (-1, -1)
+            };
+            
+            foreach (var (dx, dy) in directions)
+            {
+                var pos = Position.Add(dx, dy);
+                if (board.IsInsideBoard(pos) && (board.IsEnemyPiece(pos, Color) || board.IsEmpty(pos)))
+                    moves.Add(pos);
+            }
+
+            return moves;
+        }
+    }
+}
