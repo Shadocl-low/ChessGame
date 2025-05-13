a8adb6fd3939f3512373cd350491d0e18491dea7
Shadocl-low <vt231_poi@student.ztu.edu.ua>
2025-05-13 18:19:51 +0300

    Created abstract class Piece

---
+++ ChessGameApplication/Game/Figures/Piece.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+
+namespace ChessGameApplication.Game.Figures
+{
+    public abstract class Piece
+    {
+        public PieceColor Color { get; }
+        public Position Position { get; set; }
+
+        protected Piece(PieceColor color, Position position)
+        {
+            Color = color;
+            Position = position;
+        }
+    }
+}
