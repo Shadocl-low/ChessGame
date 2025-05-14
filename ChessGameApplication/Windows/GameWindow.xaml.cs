778a3f55e8b488c4cf89738228ec29315beea173
Shadocl-low <vt231_poi@student.ztu.edu.ua>
2025-05-14 12:29:04 +0300

    First variant of moving

--- ChessGameApplication/Windows/GameWindow.xaml.cs
+++ ChessGameApplication/Windows/GameWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Diagnostics;
+using System.Reflection;
 
 namespace ChessGameApplication.Windows
 {
@@ -19,6 +20,8 @@ namespace ChessGameApplication.Windows
     {
         private readonly SolidColorBrush lightCell = new SolidColorBrush(Color.FromRgb(240, 217, 181));
         private readonly SolidColorBrush darkCell = new SolidColorBrush(Color.FromRgb(181, 136, 99));
+        private List<Position> allowedMoves = new();
+        private Dictionary<Position, Border> positionToCellMap = new();
 
         private readonly IWindowManager Manager;
         private readonly GameManager Game;
@@ -42,8 +45,11 @@ namespace ChessGameApplication.Windows
                     {
                         Background = (row + col) % 2 == 0 ? lightCell : darkCell,
                         BorderThickness = new Thickness(0.5),
-                        BorderBrush = Brushes.Black
+                        BorderBrush = Brushes.Black,
+                        AllowDrop = true,
+                        Tag = (row, col),
                     };
+                    cell.Drop += OnCellDrop;
 
                     Piece? piece = Game.GetPiece(new Position(row, col));
                     if (piece != null)
@@ -52,19 +58,22 @@ namespace ChessGameApplication.Windows
                         {
                             Text = GetPieceSymbol(piece),
                             FontSize = 32,
+                            Tag = (row, col),
                             HorizontalAlignment = HorizontalAlignment.Center,
                             VerticalAlignment = VerticalAlignment.Center
                         };
+                        text.PreviewMouseDown += OnPieceMouseDown;
                         cell.Child = text;
                     }
 
                     ChessBoard.Children.Add(cell);
+                    positionToCellMap[new Position(row, col)] = cell;
                 }
             }
         }
 
         private void SaveGame_Click(object sender, RoutedEventArgs e) { }
-        private void BackToMenu_Click(object sender, RoutedEventArgs e) 
+        private void BackToMenu_Click(object sender, RoutedEventArgs e)
         {
             Manager.Notify(WindowActions.OpenMainMenu);
         }
@@ -72,20 +81,77 @@ namespace ChessGameApplication.Windows
         {
             return piece switch
             {
-                Pawn   p when p.Color == PieceColor.White => "♙",
-                Rook   r when r.Color == PieceColor.White => "♖",
+                Pawn p when p.Color == PieceColor.White => "♙",
+                Rook r when r.Color == PieceColor.White => "♖",
                 Knight k when k.Color == PieceColor.White => "♘",
                 Bishop b when b.Color == PieceColor.White => "♗",
-                Queen  q when q.Color == PieceColor.White => "♕",
-                King   k when k.Color == PieceColor.White => "♔",
-                Pawn   p when p.Color == PieceColor.Black => "♟",
-                Rook   r when r.Color == PieceColor.Black => "♜",
+                Queen q when q.Color == PieceColor.White => "♕",
+                King k when k.Color == PieceColor.White => "♔",
+                Pawn p when p.Color == PieceColor.Black => "♟",
+                Rook r when r.Color == PieceColor.Black => "♜",
                 Knight k when k.Color == PieceColor.Black => "♞",
                 Bishop b when b.Color == PieceColor.Black => "♝",
-                Queen  q when q.Color == PieceColor.Black => "♛",
-                King   k when k.Color == PieceColor.Black => "♚",
+                Queen q when q.Color == PieceColor.Black => "♛",
+                King k when k.Color == PieceColor.Black => "♚",
                 _ => ""
             };
+        }
+        private void OnPieceMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is TextBlock textBlock && textBlock.Tag is Position pos)
+            {
+                var piece = Game.Board.GetPieceAt(pos);
+                if (piece == null || piece.Color != Game.CurrentTurn)
+                    return;
+
+                ClearHighlights();
+
+                allowedMoves = (List<Position>)piece.GetAvailableMoves(Game.Board);
+
+                HighlightCells(allowedMoves);
+
+                DragDrop.DoDragDrop(textBlock, new DataObject("ChessPiece", pos), DragDropEffects.Move);
+            }
+        }
+        private void OnCellDrop(object sender, DragEventArgs e)
+        {
+            ClearHighlights();
+            if (e.Data.GetDataPresent("ChessPiece") && sender is Border border && border.Tag is (int toRow, int toCol))
+            {
+                var from = (Position)e.Data.GetData("ChessPiece");
+
+                if (Game.TryMakeMove(from, new Position(toRow, toCol)))
+                {
+                    CreateChessBoard();
+                }
+            }
+        }
+        private void HighlightCells(List<Position> positions)
+        {
+            foreach (var pos in positions)
+            {
+                if (positionToCellMap.TryGetValue(pos, out var border))
+                {
+                    border.Background = new SolidColorBrush(Color.FromArgb(100, 50, 200, 50));
+                }
+            }
+        }
+        private void ClearHighlights()
+        {
+            foreach (var pos in allowedMoves)
+            {
+                if (positionToCellMap.TryGetValue(pos, out var border))
+                {
+                    border.Background = GetOriginalCellColor(pos);
+                }
+            }
+            allowedMoves.Clear();
+        }
+        private Brush GetOriginalCellColor(Position pos)
+        {
+            return (pos.Row + pos.Column) % 2 == 0
+                ? Brushes.Beige
+                : Brushes.SaddleBrown;
         }
     }
 }
