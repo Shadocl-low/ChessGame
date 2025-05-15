ada987e4ca5fb401e7d9416e47d6ba4b607696d6
Shadocl-low <vt231_poi@student.ztu.edu.ua>
2025-05-15 19:47:26 +0300

    Created new Cute strategy

---
+++ ChessGameApplication/Game/PieceImageStrategies/CutePieceImageStrategy.cs
@@ -0,0 +1,35 @@
+﻿using ChessGameApplication.Game.Figures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+
+namespace ChessGameApplication.Game.PieceImageStrategies
+{
+    public class CutePieceImageStrategy : IPieceImageStrategy
+    {
+        public ImageSource GetImageForPiece(Piece piece)
+        {
+            string imageName = piece switch
+            {
+                King _ => "king",
+                Queen _ => "queen",
+                Rook _ => "rook",
+                Bishop _ => "bishop",
+                Knight _ => "knight",
+                Pawn _ => "pawn",
+                _ => throw new ArgumentException("Unknown piece type")
+            };
+
+            if (piece.Color == PieceColor.Black)
+                imageName += "_dark";
+            else
+                imageName += "_light";
+
+            return new BitmapImage(new Uri($"pack://application:,,,/images/cute/{imageName}_cute.png"));
+        }
+    }
+}
