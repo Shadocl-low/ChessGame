c09f2de2b772f4a40b84fc393b6ef3f528bff09e
Shadocl-low <vt231_poi@student.ztu.edu.ua>
2025-05-15 18:42:34 +0300

    Created Classic strategy

---
+++ ChessGameApplication/Game/PieceImageStrategies/ClassicPieceImageStrategy.cs
@@ -0,0 +1,35 @@
+﻿using ChessGameApplication.Game.Figures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+
+namespace ChessGameApplication.Game.PieceImageStrategies
+{
+    public class ClassicPieceImageStrategy : IPieceImageStrategy
+    {
+        public ImageSource GetImageForPiece(Piece piece)
+        {
+            string imageName = piece switch
+            {
+                King _ => "King",
+                Queen _ => "Queen",
+                Rook _ => "Rook",
+                Bishop _ => "Bishop",
+                Knight _ => "Knight",
+                Pawn _ => "Pawn",
+                _ => throw new ArgumentException("Unknown piece type")
+            };
+
+            if (piece.Color == PieceColor.Black)
+                imageName += "_dark";
+            else
+                imageName += "_light";
+
+            return new BitmapImage(new Uri($"pack://application:,,,/Images/Classic/{imageName}.png"));
+        }
+    }
+}
