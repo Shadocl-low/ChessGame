27d93317b9dc05feee6951aa82018c791b8d7a8f
Shadocl-low <vt231_poi@student.ztu.edu.ua>
2025-05-16 08:05:52 +0300

    Created models for saving game state

---
+++ ChessGameApplication/JsonModels/GameState.cs
@@ -0,0 +1,16 @@
+﻿using ChessGameApplication.Game;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessGameApplication.JsonModels
+{
+    public class GameState
+    {
+        public PieceColor CurrentTurn { get; set; }
+        public List<PieceModel> Pieces { get; set; } = new();
+        public bool IsCheck { get; set; }
+    }
+}
---
+++ ChessGameApplication/JsonModels/PieceModel.cs
@@ -0,0 +1,18 @@
+﻿using ChessGameApplication.Game;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessGameApplication.JsonModels
+{
+    public class PieceModel
+    {
+        public string Type { get; set; } = "";
+        public PieceColor Color { get; set; }
+        public int Row { get; set; }
+        public int Column { get; set; }
+        public bool HasMoved { get; set; }
+    }
+}
